27babc7e0e84051c0d0fa45d451fd6051c56492b
Mejia1994 <jrafa1994@gmail.com>
2021-07-10 15:35:52 -0600

    d

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 const _api = require("./api/api")
 
+const mustache = require('mustache-express');
+app.engine('html', mustache());
+app.set('view engine', 'html')
+app.set('views', __dirname + "/views");
+
 app.use(cors({optionsSuccessStatus: 200}))
+app.use(express.static('public'));
+
+app.get('/', function (reqm, res) {
+
+    res.render("index", {unix: '1451001600000', utc: 'Fri, 25 Dec 2015 00:00:00 GMT'});
+});
 
 app.use('/api/', _api);
 
-const listener = app.listen(process.env.port || 3000, function name() {
+const listener = app.listen(process.env.PORT || 3000, function name() {
     console.log("Servidor iniciado on port " + listener.address().port);
 });
